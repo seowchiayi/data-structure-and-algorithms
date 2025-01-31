85884c8a5ebafded5a595d563771638b7b103b11
seowchiayi <seowchiayi@gmail.com>
2025-02-01 00:52:02 +0800

    meta tagged questions 4

--- meta3.py
+++ meta3.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 import heapq
+import collections
 # Practice recursion with reversing singly linked list
 class SinglyLinkedList():
     def __init__(self, val: int, next=None):
@@ -117,15 +118,23 @@ def pow(base, power):
     #         i += 1
 
     # return sum
-
+    # for this (base if power % 2 else 1) - if power is odd multiply by base, if not multiply by 1
     def fast_pow(base, power):
+        print(memo)
         if power in memo:
+            
+            print("if power in memo")
+            print(memo)
+            print(power)
             return memo[power]
         if power == 0:
+            print("if power == 0")
             return 1
         elif power == 1:
+            print("elif power == 1")
             return base
         memo[power] = fast_pow(base, power // 2) * fast_pow(base, power // 2) * (base if power % 2 else 1)
+        print("ended")
         return memo[power]
 
     is_neg = power < 0
@@ -134,6 +143,70 @@ def pow(base, power):
     res = fast_pow(base, power)
 
     return 1/res if is_neg else res
+
+
+def merge_intervals(intervals: List[List[int]]):
+    res = []
+    intervals.sort()
+    for interval in intervals:
+        if len(res) > 0:
+            if res[-1][1] >= interval[0] and res[-1][1] <= interval[1]:
+                res[-1] = ([res[-1][0], interval[1]])
+            elif res[-1][1] >= interval[0] and res[-1][1] >= interval[1]:
+                continue
+            else:
+                res.append(interval)
+
+        else:
+            res.append(interval)
+
+    return res
+
+def find_first_and_last_position_of_element_in_sorted_array(nums, target):
+    # res = []
+    # for i in range(len(nums)):
+    #     if nums[i] == target:
+    #         res.append(i)
+        
+    # if len(res) == 0:
+    #     return [-1, -1]
+    # else:
+    #     return [res[0], res[-1]]
+    
+    # use binary search for Log n time complexity
+    l = 0
+    r = len(nums)
+    m = (l + r) // 2
+
+    res = []
+    # leftBias = True (we are looking for left most index for target), leftBias = False (we are looking for right most index for target)
+    
+def binary_tree_right_side_view(root: Optional[TreeNode]):
+    if not root:
+        return []
+    
+    # BFS so we are using queue
+    queue = collections.deque([root])
+    res = []
+
+    while queue:
+        level_length = len(queue)
+
+        for i in range(level_length):
+            print(f"start: level length: {level_length} {i}")
+            for item in queue:
+                print(item.val)
+            node = queue.popleft()
+            if i == level_length - 1:
+                res.append(node.val)
+                print("res")
+                print(res)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        
+    return res
 
 
 
@@ -165,9 +238,20 @@ if __name__ == "__main__":
     #print(continuous_subarray_sum(nums=[5,0,0,0], k=3))
     #print(continuous_subarray_sum(nums=[1,2,3], k=5))
     #print(pow(2, 5))
-    print(pow(2, 10))
+    #print(pow(2, 10))
     #print(pow(2, -200000000))
+    #print(merge_intervals(intervals=[[1,3],[2,6],[8,10],[15,18]]))
+    #print(merge_intervals(intervals=[[1,4],[0,4]]))
+    #print(find_first_and_last_position_of_element_in_sorted_array(nums=[5,7,7,8,8,10], target=8))
+    
+    e = TreeNode(4)
+    d = TreeNode(3)
+    c = TreeNode(5)
+    b = TreeNode(2)
 
-
-
-
+    root = TreeNode(1, b, d)
+    b.right = c
+    b.left = None
+    d.right = e
+    d.left = None
+    print(binary_tree_right_side_view(root))
